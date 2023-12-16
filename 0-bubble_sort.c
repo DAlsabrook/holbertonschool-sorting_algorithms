7891a5d5689ee70ab1df3376066e95daa4b95804
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-16 10:54:04 -0600

    added flag and 2nd loop to iterate more than once

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,19 +2,26 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t array_element = 0;
-	int element = 0, tmp;
+	int element = 0, tmp, flag = 1;
 
 	if (size < 2)
 		return;
-	while (array_element < size)
+	while (flag == 1)
 	{
-		if (array[element] > array[element + 1])
+		flag = 0;
+		element = 0;
+		array_element = 0;
+		while (array_element < size - 1)
 		{
-			tmp = array[element + 1];
-			array [element + 1] = array[element];
-			array[element] = tmp;
+			if (array[element] > array[element + 1])
+			{
+				flag = 1;
+				tmp = array[element + 1];
+				array [element + 1] = array[element];
+				array[element] = tmp;
+			}
+			element++;
+			array_element++;
 		}
-		element++;
-		array_element++;
 	}
 }
