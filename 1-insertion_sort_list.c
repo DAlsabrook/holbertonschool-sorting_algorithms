dfaf39087fc4906a31c14285d1259bb25140e128
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-16 14:00:31 -0600

    second chance

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,42 +9,39 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp = *list, *unsorted = *list;
-	int flag = 0, checker = 1;
+	int flag = 0;
 
 	if (*list == NULL)
 		return;
-	while (checker == 1)
+	while (unsorted != NULL)
 	{
-		checker = 0;
-		while (unsorted != NULL)
+		flag = 0;
+		if (unsorted->prev != NULL && unsorted->n < tmp->n)
 		{
-			flag = 0;
-			if (unsorted->prev != NULL && unsorted->n < tmp->n)
+			if (tmp->prev != NULL)
+				tmp->prev->next = unsorted;
+			if (unsorted->next != NULL)
+				unsorted->next->prev = tmp;
+			unsorted->prev = tmp->prev;
+			tmp->next = unsorted->next;
+			unsorted->next = tmp;
+			tmp->prev = unsorted;
+			if ((*list)->prev != NULL)
+				*list = unsorted;
+			if (unsorted->prev != NULL && unsorted->n < unsorted->prev->n)
 			{
-				if (tmp->prev != NULL)
-					tmp->prev->next = unsorted;
-				if (unsorted->next != NULL)
-					unsorted->next->prev = tmp;
-				unsorted->prev = tmp->prev;
-				tmp->next = unsorted->next;
-				unsorted->next = tmp;
-				tmp->prev = unsorted;
-				if ((*list)->prev != NULL)
-					*list = unsorted;
-				tmp = unsorted;
-				unsorted = unsorted->next;
-				flag = 1;
-				checker = 1;
-				print_list(*list);
+				tmp = unsorted->prev;
+				continue;
 			}
-			if (flag == 0)
-			{
-				tmp = unsorted;
-				unsorted = unsorted->next;
-			}
+			tmp = unsorted;
+			unsorted = unsorted->next;
+			flag = 1;
+			print_list(*list);
 		}
-		unsorted = *list;
-		tmp = *list;
+		if (flag == 0)
+		{
+			tmp = unsorted;
+			unsorted = unsorted->next;
+		}
 	}
-
 }
