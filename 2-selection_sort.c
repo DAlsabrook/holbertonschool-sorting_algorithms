3396763a27424ca0f226207e644ec69413214301
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-16 15:35:02 -0600

    First attempt at 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ * selection_sort - sorts an array in ascending order
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: void
+*/
+void selection_sort(int *array, size_t size)
+{
+	int value, position = 0, checker = 0, hold_pos;
+	size_t check = 0, pos = 0, flag = 0;
+
+	if (size == 0)
+		return;
+	while (pos < size - 1)
+	{
+		flag = 0;
+		value = array[position];
+		while(check < size)
+		{
+			if (array[checker] < value)
+			{
+				hold_pos = checker;
+				value = array[checker];
+				flag = 1;
+			}
+			checker++;
+			check++;
+		}
+		array[hold_pos] = array[position];
+		array[position] = value;
+		if (flag == 1)
+			print_array(array, size);
+		position++;
+		pos++;
+		checker = position;
+		check = pos;
+	}
+}
